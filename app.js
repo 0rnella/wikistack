95826da74879b38a0aaaac78883c4a903250315b
friggito <ornella.friggit@gmail.com>
2019-01-17 12:38:04 -0500

    adding view modules and setting up port

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 app.use(morgan("dev"));
-app.use(express.static(__dirname, "public"));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.get("/", (req, res) => {
+    res.send("hello world");
+    console.log("hello world");
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
